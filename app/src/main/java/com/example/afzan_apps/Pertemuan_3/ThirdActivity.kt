a6c3cc760fdcde80419bf46e07e7f9bbad7897ad
lab151-10 <lab151@gmail.com>
2026-04-10 08:29:25 +0700

    Pertemuan3_View_Binding

---
+++ app/src/main/java/com/example/afzan_apps/Pertemuan_3/ThirdActivity.kt
@@ -0,0 +1,34 @@
+package com.example.afzan_apps.Pertemuan_3
+
+import android.os.Bundle
+import android.util.Log
+import android.widget.Button
+import android.widget.EditText
+import android.widget.Toast
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+import com.example.afzan_apps.R
+import com.example.afzan_apps.databinding.ActivityThirdBinding
+
+class ThirdActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityThirdBinding
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        binding = ActivityThirdBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
+            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
+            insets
+        }
+
+
+        binding.btnKirim.setOnClickListener {
+            val noTujuan = binding.inputNoTujuan.text
+            Toast.makeText(this, "Anda telah melakukan klik pada tombol Kirim", Toast.LENGTH_SHORT).show()
+        }
+    }
+}
