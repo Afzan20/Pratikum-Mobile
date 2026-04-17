c30e586d5bdbd0e0049a7fa8923e214a8ee3d64d
lab151-10 <lab151@gmail.com>
2026-04-17 07:59:26 +0700

    Pertemuan4_WarmingUp

--- app/src/main/java/com/example/afzan_apps/Pertemuan_4/FourthActivity.kt
+++ app/src/main/java/com/example/afzan_apps/Pertemuan_4/FourthActivity.kt
@@ -1,4 +1,4 @@
-package com.example.afzan_apps
+package com.example.afzan_apps.Pertemuan_4
 
 import android.content.Intent
 import android.os.Bundle
@@ -6,16 +6,17 @@ import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
-import com.example.afzan_apps.Pertemuan_4.FourthActivity
-import com.example.afzan_apps.databinding.ActivityMainBinding
+import com.example.afzan_apps.MainActivity
+import com.example.afzan_apps.Pertemuan_3.ThirdResultActivity
+import com.example.afzan_apps.R
+import com.example.afzan_apps.databinding.ActivityFourthBinding
 
-class MainActivity : AppCompatActivity() {
-    private lateinit var binding: ActivityMainBinding
+class FourthActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityFourthBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
-        setContentView(R.layout.activity_main)
-        binding = ActivityMainBinding.inflate(layoutInflater)
+        binding = ActivityFourthBinding.inflate(layoutInflater)
         setContentView(binding.root)
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
@@ -24,8 +25,8 @@ class MainActivity : AppCompatActivity() {
         }
 
 
-        binding.btnSelanjutnya.setOnClickListener {
-            val intent = Intent(this, FourthActivity::class.java)
+        binding.btnKembali.setOnClickListener {
+            val intent = Intent(this, MainActivity::class.java)
             startActivity(intent)
         }
     }
